7e7e229dd5f5ad8f4e47ee238039da300659fd7b
Luis <luisslillo_90@hotmail.es>
2019-08-16 21:44:18 +0100

    changed char in mobile skill

--- src/app/components/charts/polar-area/polar-area.component.ts
+++ src/app/components/charts/polar-area/polar-area.component.ts
@@ -9,22 +9,50 @@ import { ChartType } from 'chart.js';
 })
 export class PolarAreaComponent implements OnInit {
 // PolarArea
-public polarAreaChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail Sales', 'Telesales', 'Corporate Sales'];
-public polarAreaChartData: SingleDataSet = [300, 500, 100, 40, 120];
-public polarAreaLegend = false;
+public ionicValue = 0;
+public pwaValue = 0;
+public flutterValue = 0;
+public polarAreaChartLabels: Label[] = ['PWA', 'IONIC', 'Flutter'];
+public polarAreaChartData: SingleDataSet = [this.pwaValue, this.ionicValue, this.flutterValue];
+public polarAreaLegend = true;
 
 public polarAreaChartType: ChartType = 'polarArea';
 
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.showElements();
+    }, 10);
   }
+
+  async showElements() {
+    let time = 1000; // THis is the time when start the process
+    const latency = 1500; // This is the latency of each element
+    await setTimeout(() => {
+      this.ionicValue = 500;
+      this.changeData();
+    }, time);
+    await setTimeout(() => {
+      this.pwaValue = 400;
+      this.changeData();
+    }, time = time + latency);
+    await setTimeout(() => {
+      this.flutterValue = 200;
+      this.changeData();
+    }, time = time + latency);
+  }
+
+  changeData() {
+    this.polarAreaChartData = [this.pwaValue, this.ionicValue, this.flutterValue];
+  }
+
 
     // events
     public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
       console.log(event, active);
     }
-  
+
     public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
       console.log(event, active);
     }
